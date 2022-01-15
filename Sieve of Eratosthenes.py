2240af8e6739530320c42f3e98706e99d650592e
kaki1013 <lmklove0@naver.com>
2022-01-16 02:46:55 +0900

    에라토스테네스의 체 추가

---
+++ Sieve of Eratosthenes.py
@@ -0,0 +1,17 @@
+# n 이하의 소수 찾기
+# 출처: https://ko.wikipedia.org/wiki/%EC%97%90%EB%9D%BC%ED%86%A0%EC%8A%A4%ED%85%8C%EB%84%A4%EC%8A%A4%EC%9D%98_%EC%B2%B4
+def prime_list(n):
+    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    sieve = [True] * (n+1)
+
+    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i]:  # i가 소수인 경우
+            for j in range(i + i, n+1, i):  # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return [i for i in range(2, n+1) if sieve[i]]
+
+print(prime_list(11))
